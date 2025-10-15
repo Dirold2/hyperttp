c31dbfdcd9239c0cbc3ecb422a343cd58d8e5110
dirold2 <mr.did199@mail.ru>
2025-10-15 14:48:16 +0300

    v0.1.0

--- src/Hyperttp/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  HttpClientImproved,
-  QueueManager,
-  CacheManager,
-  RateLimiter,
-} from "./Core";
-export { default as Request } from "./Request";
-export { PreparedRequest } from "./Request";
-export { default as UrlExtractor } from "./UrlExtractor";
