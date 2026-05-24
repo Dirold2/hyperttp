d2b72bba79dd382d77a30635b1a8b42143de30a1
dirold2 <i@dirold2.ru>
2026-05-25 02:21:53 +0300

    v0.3.13

---
+++ src/augmentations.ts
@@ -0,0 +1,8 @@
+import "@hyperttp/core";
+import "@hyperttp/cache";
+import "@hyperttp/parser";
+import "@hyperttp/queue";
+import "@hyperttp/ratelimit";
+import "@hyperttp/inflight";
+import "@hyperttp/interceptors";
+import "@hyperttp/metrics";
