906f5572e5f589f5068e255c1f77ad0ba3a00e13
dirold2 <i@dirold2.ru>
2026-05-25 02:50:10 +0300

    v0.3.15

---
+++ src/Types/global.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="@hyperttp/core" />
+/// <reference types="@hyperttp/cache" />
+/// <reference types="@hyperttp/queue" />
+/// <reference types="@hyperttp/ratelimit" />
+/// <reference types="@hyperttp/interceptors" />
+/// <reference types="@hyperttp/metrics" />
